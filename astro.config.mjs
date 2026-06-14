cd979fb6a67806b7a8db66cab8f959d3079d04eb
Jose Carrizo <carrizoja@gmail.com>
2026-06-14 20:44:06 -0300

    chore: switch from vercel adapter to netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   },
